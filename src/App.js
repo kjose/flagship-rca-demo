ea00e85a82c5a6dd4a725bdd9058b3e97143a570
Kévin JOSE <kevin.jose.92@gmail.com>
2022-09-20 16:21:43 +0200

    add features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { FlagshipProvider } from "@flagship.io/react-sdk";
+import { v4 as uuidv4 } from 'uuid';
+import { useFsFlag } from "@flagship.io/react-sdk";
 
 function App() {
+  const feature01enabled = useFsFlag("0.1",false);
+  const feature10enabled = useFsFlag("1.0",false);
+  const feature11enabled = useFsFlag("1.1",false);
+
+  console.log(feature01enabled);
+  console.log(feature10enabled);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FlagshipProvider
+        envId={process.env.REACT_APP_FS_ENV_ID}
+        apiKey={process.env.REACT_APP_FS_API_KEY}
+        visitorData={{
+          id: uuidv4(),
+          context: {
+            // some context
+          },
+          isAuthenticated: false,
+        }}
+      >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="features">
+            {feature01enabled.getValue() && <span>Feature 0.1</span>}
+            {feature10enabled.getValue() && <span>Feature 1.0</span>}
+            {feature11enabled.getValue() && <span>Feature 1.1</span>}
+          </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </FlagshipProvider>
     </div>
   );
 }
