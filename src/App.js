92155f2965f67fd90c945fc2e40ac80deaaa754b
Kévin JOSE <kevin.jose.92@gmail.com>
2022-09-21 17:35:42 +0200

    1.0.1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useFsFlag } from "@flagship.io/react-sdk";
 
 function App() {
   const feature1_0_0_enabled = useFsFlag("1.0.0",false);
+  const feature1_0_1_enabled = useFsFlag("1.0.1",false);
 
   return (
     <div className="App">
@@ -11,6 +12,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <div className="features">
           {feature1_0_0_enabled.getValue() && <span>Feature 1.0.0</span>}
+        </div>
+        <div className="features">
+          {feature1_0_1_enabled.getValue() && <span>Feature 1.0.1</span>}
         </div>
         <a
           className="App-link"
